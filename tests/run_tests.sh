850db3975d3d34313eeb93f2986ff5293b2496bf
Watson <wtsnjp@gmail.com>
2017-04-21 17:18:54 +0900

    fix run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,13 +8,15 @@ TMPDIR=$PWDF/tmp
 ENGINES=("latex" "platex" "uplatex" "xelatex" "lualatex")
 TESTNAMES=("standard_dp" "memoization")
 
-mkdir $TMPDIR && cd $TMPDIR
+mkdir $TMPDIR
+cp *.sty $TMPDIR
+cd $TMPDIR
 
 for t in ${TESTNAMES[@]}; do
   for e in ${ENGINES[@]}; do
     $e $TESTDIR/$t.tex && diff $t.dbg $TESTDIR/$t.txt
     if [ $? = 0 ]; then
-      rm -rf $TMPDIR/*
+      rm -rf $TMPDIR/$t.*
     else
       exit 1
     fi
