18b43c44796431f1226c4557076aaf9f6116287e
Watson <wtsnjp@gmail.com>
2017-04-21 14:40:19 +0900

    add tests

---
+++ tests/run_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PWDF=`pwd`
+
+TESTDIR=$PWDF/tests
+TMPDIR=$PWDF/tmp
+
+ENGINES=("latex" "platex" "uplatex" "xelatex" "lualatex")
+TESTNAMES=("standard_dp" "memoization")
+
+mkdir $TMPDIR && cd $TMPDIR
+
+for t in ${TESTNAMES[@]}; do
+  for e in ${ENGINES[@]}; do
+    $e $TESTDIR/$t.tex && diff $t.dbg $TESTDIR/$t.txt
+    if [ $? = 0 ]; then
+      rm -rf $TMPDIR/*
+    else
+      exit 1
+    fi
+  done
+done
+
+rm -rf $TMPDIR
